0f1f333c9b1d4612c81b6d8722ea2cd0bbe7e5ac
Petr Jelinek <pjmodos@pjmodos.net>
2017-04-15 03:27:32 +0200

    Set range table for CopyFrom in tablesync
    This changes the mechanism of how range table is passed to CopyFrom
    executor state. We used to generate the range table and one entry for
    the relation manually inside DoCopy.Now we use
    addRangeTableEntryForRelation to setup the range table and relation
    entry for the ParseState which is then passed down by BeginCopyFrom.

--- src/backend/replication/logical/tablesync.c
+++ src/backend/replication/logical/tablesync.c
@@ -93,6 +93,8 @@
 
 #include "commands/copy.h"
 
+#include "parser/parse_relation.h"
+
 #include "replication/logicallauncher.h"
 #include "replication/logicalrelation.h"
 #include "replication/walreceiver.h"
@@ -648,6 +650,8 @@ copy_table(Relation rel)
 	StringInfoData		cmd;
 	CopyState	cstate;
 	List	   *attnamelist;
+	ParseState *pstate;
+	RangeTblEntry *rte;
 
 	/* Get the publisher relation info. */
 	fetch_remote_table_info(get_namespace_name(RelationGetNamespace(rel)),
@@ -674,9 +678,14 @@ copy_table(Relation rel)
 
 	copybuf = makeStringInfo();
 
-	/* Create CopyState for ingestion of the data from publisher. */
+	pstate = make_parsestate(NULL);
+	pstate->p_target_relation = rel;
+	rte = addRangeTableEntryForRelation(pstate, pstate->p_target_relation,
+										NULL, false, false);
+	pstate->p_target_rangetblentry = rte;
+
 	attnamelist = make_copy_attnamelist(relmapentry);
-	cstate = BeginCopyFrom(NULL, rel, NULL, false, copy_read_data, attnamelist, NIL);
+	cstate = BeginCopyFrom(pstate, rel, NULL, false, copy_read_data, attnamelist, NIL);
 
 	/* Do the copy */
 	(void) CopyFrom(cstate);
