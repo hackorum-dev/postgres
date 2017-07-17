958dc301d461974fbd99ba518419ad8e093728ba
hackorum <git@hackorum.dev>
2017-07-17 19:35:48 +0000

    Apply experiment.patch

--- src/backend/commands/tablecmds.c
+++ src/backend/commands/tablecmds.c
@@ -5750,6 +5750,9 @@ ATExecCmd(List **wqueue, AlteredTableInfo *tab,
 			break;
 	}
 
+	/* Experiment for bug report 14691.  This doesn't do anything useful. */
+	CheckTableForSerializableConflictIn(rel);
+
 	/*
 	 * Report the subcommand to interested event triggers.
 	 */
