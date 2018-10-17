8da6bd2f70cf520a2bd66c64c97da2ed86423805
hackorum <git@hackorum.dev>
2018-10-17 01:39:02 +0000

    Apply tuple-data-record-v1.patch

--- contrib/pageinspect/heapfuncs.c
+++ contrib/pageinspect/heapfuncs.c
@@ -277,33 +277,22 @@ heap_page_items(PG_FUNCTION_ARGS)
 }
 
 /*
- * tuple_data_split_internal
+ * parse_tuple_attr_info
  *
- * Split raw tuple data taken directly from a page into an array of bytea
- * elements. This routine does a lookup on NULL values and creates array
- * elements accordingly. This is a reimplementation of nocachegetattr()
- * in heaptuple.c simplified for educational purposes.
+ * Read raw tuple data and determine attribute offset, length, and null status.
+ * This is a reimplementation of nocachegetattr() in heaptuple.c simplified
+ * for educational purposes.
  */
-static Datum
-tuple_data_split_internal(Oid relid, char *tupdata,
-						  uint16 tupdata_len, uint16 t_infomask,
-						  uint16 t_infomask2, bits8 *t_bits,
-						  bool do_detoast)
+static void
+parse_tuple_attr_info(TupleDesc tupdesc, char *tupdata, uint16 tupdata_len,
+		uint16 t_infomask, uint16 t_infomask2, bits8 *t_bits,
+		bool *is_null, int *offsets, int *lengths, bool *varlen)
 {
-	ArrayBuildState *raw_attrs;
-	int			nattrs;
 	int			i;
-	int			off = 0;
-	Relation	rel;
-	TupleDesc	tupdesc;
+	int nattrs;
+	int off = 0;
 
-	/* Get tuple descriptor from relation OID */
-	rel = relation_open(relid, AccessShareLock);
-	tupdesc = RelationGetDescr(rel);
-
-	raw_attrs = initArrayResult(BYTEAOID, CurrentMemoryContext, false);
 	nattrs = tupdesc->natts;
-
 	if (nattrs < (t_infomask2 & HEAP_NATTS_MASK))
 		ereport(ERROR,
 				(errcode(ERRCODE_DATA_CORRUPTED),
@@ -312,8 +301,7 @@ tuple_data_split_internal(Oid relid, char *tupdata,
 	for (i = 0; i < nattrs; i++)
 	{
 		Form_pg_attribute attr;
-		bool		is_null;
-		bytea	   *attr_data = NULL;
+		bool attr_isnull;
 
 		attr = TupleDescAttr(tupdesc, i);
 
@@ -324,16 +312,20 @@ tuple_data_split_internal(Oid relid, char *tupdata,
 		 * (t_infomask2 & HEAP_NATTS_MASK) should be treated as NULL.
 		 */
 		if (i >= (t_infomask2 & HEAP_NATTS_MASK))
-			is_null = true;
+			attr_isnull = true;
 		else
-			is_null = (t_infomask & HEAP_HASNULL) && att_isnull(i, t_bits);
+			attr_isnull = (t_infomask & HEAP_HASNULL) && att_isnull(i, t_bits);
 
-		if (!is_null)
+		is_null[i] = attr_isnull;
+
+		if (!attr_isnull)
 		{
-			int			len;
+			int len;
 
 			if (attr->attlen == -1)
 			{
+				if (varlen)
+					varlen[i] = true;
 				off = att_align_pointer(off, attr->attalign, -1,
 										tupdata + off);
 
@@ -362,33 +354,169 @@ tuple_data_split_internal(Oid relid, char *tupdata,
 						(errcode(ERRCODE_DATA_CORRUPTED),
 						 errmsg("unexpected end of tuple data")));
 
-			if (attr->attlen == -1 && do_detoast)
-				attr_data = DatumGetByteaPCopy(tupdata + off);
-			else
-			{
-				attr_data = (bytea *) palloc(len + VARHDRSZ);
-				SET_VARSIZE(attr_data, len + VARHDRSZ);
-				memcpy(VARDATA(attr_data), tupdata + off, len);
-			}
+			lengths[i] = len;
+			offsets[i] = off;
 
 			off = att_addlength_pointer(off, attr->attlen,
 										tupdata + off);
 		}
-
-		raw_attrs = accumArrayResult(raw_attrs, PointerGetDatum(attr_data),
-									 is_null, BYTEAOID, CurrentMemoryContext);
-		if (attr_data)
-			pfree(attr_data);
 	}
 
 	if (tupdata_len != off)
 		ereport(ERROR,
 				(errcode(ERRCODE_DATA_CORRUPTED),
 				 errmsg("end of tuple reached without looking at all its data")));
+}
+
+
+/*
+ * tuple_data_split_internal
+ *
+ * Split raw tuple data taken directly from a page into an array of bytea
+ * elements. This routine does a lookup on NULL values and creates array
+ * elements accordingly.
+ */
+static Datum
+tuple_data_split_internal(Oid relid, char *tupdata,
+			uint16 tupdata_len, uint16 t_infomask,
+			uint16 t_infomask2, bits8 *t_bits,
+			bool do_detoast)
+{
+	ArrayBuildState *raw_attrs;
+	int			nattrs;
+	int			i;
+	Relation	rel;
+	TupleDesc	tupdesc;
+	bool *isnull;
+	bool *varlen;
+	int *offsets;
+	int *lengths;
+
+	/* Get tuple descriptor from relation OID */
+	rel = relation_open(relid, AccessShareLock);
+	tupdesc = RelationGetDescr(rel);
+
+	raw_attrs = initArrayResult(BYTEAOID, CurrentMemoryContext, false);
+	nattrs = tupdesc->natts;
+	isnull = (bool *)palloc(sizeof(bool) * nattrs);
+	varlen = (bool *)palloc0(sizeof(bool) * nattrs);
+	offsets = (int *)palloc(sizeof(int) * nattrs);
+	lengths = (int *)palloc(sizeof(int) * nattrs);
+
+	parse_tuple_attr_info(tupdesc, tupdata, tupdata_len, t_infomask, t_infomask2, t_bits,
+		isnull, offsets, lengths, varlen);
+
+	/* Fetch all tuple attribute values */
+	for (i = 0; i < nattrs; i++)
+	{
+		bytea *attr_data = NULL;
+			if (varlen[i] && do_detoast)
+				attr_data = DatumGetByteaPCopy(tupdata + offsets[i]);
+			else
+			{
+				attr_data = (bytea *) palloc(lengths[i] + VARHDRSZ);
+				SET_VARSIZE(attr_data, lengths[i] + VARHDRSZ);
+				memcpy(VARDATA(attr_data), tupdata + offsets[i], lengths[i]);
+			}
+
+		raw_attrs = accumArrayResult(raw_attrs, PointerGetDatum(attr_data),
+									 isnull[i], BYTEAOID, CurrentMemoryContext);
+		if (attr_data)
+			pfree(attr_data);
+	}
 
 	relation_close(rel, AccessShareLock);
 
 	return makeArrayResult(raw_attrs, CurrentMemoryContext);
+}
+
+/*
+ * tuple_data_record_internal
+ *
+ * Split raw tuple data taken directly from a page into record of the page's
+ * relation's row tyoe.
+ */
+static Datum
+tuple_data_split_internal_record(Oid relid, char *tupdata,
+			uint16 tupdata_len, uint16 t_infomask,
+			uint16 t_infomask2, bits8 *t_bits)
+{
+	int i;
+	int nattrs;
+	Relation	rel;
+	TupleDesc	tupdesc;
+	Datum *values;
+	bool *isnull;
+	int *offsets;
+	int *lengths;
+
+	/* Get tuple descriptor from relation OID */
+	rel = relation_open(relid, AccessShareLock);
+	tupdesc = RelationGetDescr(rel);
+
+	nattrs = tupdesc->natts;
+	isnull = (bool *)palloc(sizeof(bool) * nattrs);
+	offsets = (int *)palloc(sizeof(int) * nattrs);
+	lengths = (int *)palloc(sizeof(int) * nattrs);
+	values = palloc(sizeof(Datum) * nattrs);
+
+	parse_tuple_attr_info(tupdesc, tupdata, tupdata_len, t_infomask, t_infomask2, t_bits,
+		isnull, offsets, lengths, NULL);
+
+	/* Fetch all tuple attribute values */
+	for (i = 0; i < tupdesc->natts; i++)
+	{
+		values[i] = fetchatt(TupleDescAttr(tupdesc, i), tupdata + offsets[i]);
+	}
+
+	relation_close(rel, AccessShareLock);
+
+	return HeapTupleGetDatum(heap_form_tuple(tupdesc, values, isnull));
+}
+
+/*
+ * parse_and_verify_t_bits
+ *
+ * Convert t_bits string back to the bits8 array as represented in the
+ * tuple header and error check results.
+ */
+static bits8 *
+parse_and_verify_t_bits(char *t_bits_str, uint16 t_infomask, uint16 t_infomask2)
+{
+	bits8	*t_bits = NULL;
+
+	if (t_infomask & HEAP_HASNULL)
+	{
+		int			bits_str_len;
+		int			bits_len;
+
+		bits_len = BITMAPLEN(t_infomask2 & HEAP_NATTS_MASK) * BITS_PER_BYTE;
+		if (!t_bits_str)
+			ereport(ERROR,
+					(errcode(ERRCODE_DATA_CORRUPTED),
+					 errmsg("argument of t_bits is null, but it is expected to be null and %d character long",
+							bits_len)));
+
+		bits_str_len = strlen(t_bits_str);
+		if (bits_len != bits_str_len)
+			ereport(ERROR,
+					(errcode(ERRCODE_DATA_CORRUPTED),
+					 errmsg("unexpected length of t_bits %u, expected %d",
+							bits_str_len, bits_len)));
+
+		/* do the conversion */
+		t_bits = text_to_bits(t_bits_str, bits_str_len);
+	}
+	else
+	{
+		if (t_bits_str)
+			ereport(ERROR,
+					(errcode(ERRCODE_DATA_CORRUPTED),
+					 errmsg("t_bits string is expected to be NULL, but instead it is %zu bytes length",
+							strlen(t_bits_str))));
+	}
+
+	return t_bits;
 }
 
 /*
@@ -429,40 +557,7 @@ tuple_data_split(PG_FUNCTION_ARGS)
 	if (!raw_data)
 		PG_RETURN_NULL();
 
-	/*
-	 * Convert t_bits string back to the bits8 array as represented in the
-	 * tuple header.
-	 */
-	if (t_infomask & HEAP_HASNULL)
-	{
-		int			bits_str_len;
-		int			bits_len;
-
-		bits_len = BITMAPLEN(t_infomask2 & HEAP_NATTS_MASK) * BITS_PER_BYTE;
-		if (!t_bits_str)
-			ereport(ERROR,
-					(errcode(ERRCODE_DATA_CORRUPTED),
-					 errmsg("argument of t_bits is null, but it is expected to be null and %d character long",
-							bits_len)));
-
-		bits_str_len = strlen(t_bits_str);
-		if (bits_len != bits_str_len)
-			ereport(ERROR,
-					(errcode(ERRCODE_DATA_CORRUPTED),
-					 errmsg("unexpected length of t_bits %u, expected %d",
-							bits_str_len, bits_len)));
-
-		/* do the conversion */
-		t_bits = text_to_bits(t_bits_str, bits_str_len);
-	}
-	else
-	{
-		if (t_bits_str)
-			ereport(ERROR,
-					(errcode(ERRCODE_DATA_CORRUPTED),
-					 errmsg("t_bits string is expected to be NULL, but instead it is %zu bytes length",
-							strlen(t_bits_str))));
-	}
+	t_bits = parse_and_verify_t_bits(t_bits_str, t_infomask, t_infomask2);
 
 	/* Split tuple data */
 	res = tuple_data_split_internal(relid, (char *) raw_data + VARHDRSZ,
@@ -474,4 +569,44 @@ tuple_data_split(PG_FUNCTION_ARGS)
 		pfree(t_bits);
 
 	PG_RETURN_ARRAYTYPE_P(res);
+}
+
+PG_FUNCTION_INFO_V1(tuple_data_record);
+
+Datum
+tuple_data_record(PG_FUNCTION_ARGS)
+{
+	Oid	relid;
+	bytea *raw_data;
+	uint16 t_infomask;
+	uint16 t_infomask2;
+	char *t_bits_str;
+	bits8 *t_bits = NULL;
+	Datum res;
+
+	relid = PG_GETARG_OID(0);
+	raw_data = PG_ARGISNULL(1) ? NULL : PG_GETARG_BYTEA_P(1);
+	t_infomask = PG_GETARG_INT16(2);
+	t_infomask2 = PG_GETARG_INT16(3);
+	t_bits_str = PG_ARGISNULL(4) ? NULL :
+		text_to_cstring(PG_GETARG_TEXT_PP(4));
+
+	if (!superuser())
+		ereport(ERROR,
+				(errcode(ERRCODE_INSUFFICIENT_PRIVILEGE),
+				 errmsg("must be superuser to use raw page functions")));
+
+	if (!raw_data)
+		PG_RETURN_NULL();
+
+	t_bits = parse_and_verify_t_bits(t_bits_str, t_infomask, t_infomask2);
+
+	res = tuple_data_split_internal_record(relid, (char *) raw_data + VARHDRSZ,
+									VARSIZE(raw_data) - VARHDRSZ,
+									t_infomask, t_infomask2, t_bits);
+
+	if (t_bits)
+		pfree(t_bits);
+
+	return res;
 }
