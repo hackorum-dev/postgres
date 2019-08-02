d7494f1020efeb50a58f3fa261e60165729a2319
Thomas Munro <thomas.munro@gmail.com>
2019-08-02 17:29:34 +1200

    Add SQL type xid8 to expose FullTransactionId to users.
    Similar to xid, but 64 bits wide.  This new type is suitable for use
    in various system views and administration functions.
    
    Author: Thomas Munro
    Discussion: https://postgr.es/m/https://www.postgresql.org/message-id/20190725000636.666m5mad25wfbrri%40alap3.anarazel.de

--- src/include/access/transam.h
+++ src/include/access/transam.h
@@ -72,6 +72,16 @@ FullTransactionIdFromEpochAndXid(uint32 epoch, TransactionId xid)
 	return result;
 }
 
+static inline FullTransactionId
+FullTransactionIdFromU64(uint64 value)
+{
+	FullTransactionId result;
+
+	result.value = value;
+
+	return result;
+}
+
 /* advance a transaction ID variable, handling wraparound correctly */
 #define TransactionIdAdvance(dest)	\
 	do { \
