11d18b729f2031cb61f40d20bcc452481e66b1c2
hackorum <git@hackorum.dev>
2019-12-01 19:05:56 +0000

    Apply oom-fix-95.patch

--- src/backend/optimizer/path/pathkeys.c
+++ src/backend/optimizer/path/pathkeys.c
@@ -1560,8 +1560,8 @@ find_mergeclauses_for_outer_pathkeys(PlannerInfo *root,
 	{
 		PathKey    *pathkey = (PathKey *) lfirst(i);
 		EquivalenceClass *pathkey_ec = pathkey->pk_eclass;
-		List	   *matched_restrictinfos = NIL;
 		ListCell   *j;
+		bool		found = false;
 
 		/*----------
 		 * A mergejoin clause matches a pathkey if it has the same EC.
@@ -1607,8 +1607,12 @@ find_mergeclauses_for_outer_pathkeys(PlannerInfo *root,
 
 			clause_ec = rinfo->outer_is_left ?
 				rinfo->left_ec : rinfo->right_ec;
-			if (clause_ec == pathkey_ec)
-				matched_restrictinfos = lappend(matched_restrictinfos, rinfo);
+
+			if (clause_ec != pathkey_ec)
+				continue;
+
+			mergeclauses = lappend(mergeclauses, rinfo);
+			found = true;
 		}
 
 		/*
@@ -1616,14 +1620,8 @@ find_mergeclauses_for_outer_pathkeys(PlannerInfo *root,
 		 * sort-key positions in the pathkeys are useless.  (But we can still
 		 * mergejoin if we found at least one mergeclause.)
 		 */
-		if (matched_restrictinfos == NIL)
+		if (!found)
 			break;
-
-		/*
-		 * If we did find usable mergeclause(s) for this sort-key position,
-		 * add them to result list.
-		 */
-		mergeclauses = list_concat(mergeclauses, matched_restrictinfos);
 	}
 
 	return mergeclauses;
