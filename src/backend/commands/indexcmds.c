a8621e591721157dc64129208638447565193364
Daniel Gustafsson <daniel@yesql.se>
2020-03-05 21:29:02 +0100

    Remove handling of removed _ops classes
    opclasses removed since long were silently handled by dropping them
    for the default, but it's now time to remove this workaround ans the
    opclasses in question were removed a long time ago.

--- src/backend/commands/indexcmds.c
+++ src/backend/commands/indexcmds.c
@@ -1850,34 +1850,6 @@ ResolveOpClass(List *opclass, Oid attrType,
 	Oid			opClassId,
 				opInputType;
 
-	/*
-	 * Release 7.0 removed network_ops, timespan_ops, and datetime_ops, so we
-	 * ignore those opclass names so the default *_ops is used.  This can be
-	 * removed in some later release.  bjm 2000/02/07
-	 *
-	 * Release 7.1 removes lztext_ops, so suppress that too for a while.  tgl
-	 * 2000/07/30
-	 *
-	 * Release 7.2 renames timestamp_ops to timestamptz_ops, so suppress that
-	 * too for awhile.  I'm starting to think we need a better approach. tgl
-	 * 2000/10/01
-	 *
-	 * Release 8.0 removes bigbox_ops (which was dead code for a long while
-	 * anyway).  tgl 2003/11/11
-	 */
-	if (list_length(opclass) == 1)
-	{
-		char	   *claname = strVal(linitial(opclass));
-
-		if (strcmp(claname, "network_ops") == 0 ||
-			strcmp(claname, "timespan_ops") == 0 ||
-			strcmp(claname, "datetime_ops") == 0 ||
-			strcmp(claname, "lztext_ops") == 0 ||
-			strcmp(claname, "timestamp_ops") == 0 ||
-			strcmp(claname, "bigbox_ops") == 0)
-			opclass = NIL;
-	}
-
 	if (opclass == NIL)
 	{
 		/* no operator class specified, so find the default */
