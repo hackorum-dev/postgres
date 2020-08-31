02fdcc71cf984bb83d138273502a2a5a6d805f5b
Asim R P <pasim@vmware.com>
2020-08-31 19:41:02 +0530

    Speculative insert isolation test spec using fault injector
    This spec simulates a specific interleaving of concurrent speculative
    inserts that is rather cumbersome to achieve without injecting faults.
    The interleaving is such that one speculative insert should encounter
    conflict only after inserting a tuple into heap relation but before
    inserting it into index relation.
    
    Discussion: https://www.postgresql.org/message-id/CAAKRu_a7hbyrk%3DwveHYhr4LbcRnRCG%3DyPUVoQYB9YO1CdUBE9Q%40mail.gmail.com

--- src/backend/access/heap/heapam.c
+++ src/backend/access/heap/heapam.c
@@ -64,6 +64,7 @@
 #include "storage/spin.h"
 #include "storage/standby.h"
 #include "utils/datum.h"
+#include "utils/faultinjector.h"
 #include "utils/inval.h"
 #include "utils/lsyscache.h"
 #include "utils/relcache.h"
@@ -5576,6 +5577,7 @@ heap_abort_speculative(Relation relation, ItemPointer tid)
 	TransactionId prune_xid;
 
 	Assert(ItemPointerIsValid(tid));
+	SIMPLE_FAULT_INJECTOR("heap_abort_speculative");
 
 	block = ItemPointerGetBlockNumber(tid);
 	buffer = ReadBuffer(relation, block);
