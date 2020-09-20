b2b0ab2739002c2de4eb2f099236f99d591dbe4d
hackorum <git@hackorum.dev>
2020-09-20 23:09:56 +0000

    Apply gd_is_null.patch

--- src/backend/optimizer/plan/planner.c
+++ src/backend/optimizer/plan/planner.c
@@ -4066,6 +4066,8 @@ create_grouping_paths(PlannerInfo *root,
 	{
 		int			flags = 0;
 		GroupPathExtraData extra;
+		
+		Assert(gd != NULL);
 
 		/*
 		 * Determine whether it's possible to perform sort-based
