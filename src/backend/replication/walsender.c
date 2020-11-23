892b6253c0736b7d7afaf6cb75efb205074492ea
Alvaro Herrera <alvherre@alvh.no-ip.org>
2020-11-23 17:51:34 -0300

    Don't loop forever in WalSndDone
    For a walsender that hasn't sent anything, the "replicatedPtr" as
    computed for shutdown is not valid, so the comparison to sentPtr fails.
    Make sure to only compare if replicatedPtr is valid.

--- src/backend/replication/walsender.c
+++ src/backend/replication/walsender.c
@@ -3822,7 +3822,8 @@ WalSndDone(WalSndSendDataCallback send_data)
 	replicatedPtr = XLogRecPtrIsValid(MyWalSnd->flush) ?
 		MyWalSnd->flush : MyWalSnd->write;
 
-	if (WalSndCaughtUp && sentPtr == replicatedPtr &&
+	if (WalSndCaughtUp &&
+		(XLogRecPtrIsInvalid(replicatedPtr) || sentPtr == replicatedPtr) &&
 		!pq_is_send_pending())
 	{
 		QueryCompletion qc;
