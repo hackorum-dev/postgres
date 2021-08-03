77247958d95ffd8ef81fae6487f3540905cd1602
Mark Dilger <mark.dilger@enterprisedb.com>
2021-08-03 09:34:19 -0700

    Allow SET and ALTER SYSTEM SET per role.
    Lay the groundwork for delegating to built-in roles the privilege to
    perform SET and ALTER SYSTEM SET on GUC variables which otherwise
    can only be changed by superuser.  Do not delegate any variables
    here.  We anticipate follow-on patches to reduce the number of
    operations which require superuser by creating built-in roles with
    privilege to manage portions of the set of all GUC variables.

--- src/include/utils/guc.h
+++ src/include/utils/guc.h
@@ -199,6 +199,10 @@ typedef enum
 #define GUC_QUALIFIER_SEPARATOR '.'
 
 /*
+ * privilege bits required to modify a GUC variable
+ */
+
+/*
  * bit values in "flags" of a GUC variable
  */
 #define GUC_LIST_INPUT			0x0001	/* input can be list format */
@@ -294,6 +298,7 @@ extern void DefineCustomBoolVariable(const char *name,
 									 bool *valueAddr,
 									 bool bootValue,
 									 GucContext context,
+									 int privileges,
 									 int flags,
 									 GucBoolCheckHook check_hook,
 									 GucBoolAssignHook assign_hook,
@@ -307,6 +312,7 @@ extern void DefineCustomIntVariable(const char *name,
 									int minValue,
 									int maxValue,
 									GucContext context,
+									int privileges,
 									int flags,
 									GucIntCheckHook check_hook,
 									GucIntAssignHook assign_hook,
@@ -320,6 +326,7 @@ extern void DefineCustomRealVariable(const char *name,
 									 double minValue,
 									 double maxValue,
 									 GucContext context,
+									 int privileges,
 									 int flags,
 									 GucRealCheckHook check_hook,
 									 GucRealAssignHook assign_hook,
@@ -331,6 +338,7 @@ extern void DefineCustomStringVariable(const char *name,
 									   char **valueAddr,
 									   const char *bootValue,
 									   GucContext context,
+									   int privileges,
 									   int flags,
 									   GucStringCheckHook check_hook,
 									   GucStringAssignHook assign_hook,
@@ -343,6 +351,7 @@ extern void DefineCustomEnumVariable(const char *name,
 									 int bootValue,
 									 const struct config_enum_entry *options,
 									 GucContext context,
+									 int privileges,
 									 int flags,
 									 GucEnumCheckHook check_hook,
 									 GucEnumAssignHook assign_hook,
