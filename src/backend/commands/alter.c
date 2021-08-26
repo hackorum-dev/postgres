22eb1e938e2b9d8f2fe7ac6b442da5a27b1c797b
Mark Dilger <mark.dilger@enterprisedb.com>
2021-08-26 15:42:27 -0700

    Adding owners to roles.
    All roles now have owners.  By default, roles belong to the role
    that created them, and initdb-time roles are owned by POSTGRES.

--- src/backend/commands/alter.c
+++ src/backend/commands/alter.c
@@ -839,6 +839,9 @@ ExecAlterOwnerStmt(AlterOwnerStmt *stmt)
 		case OBJECT_DATABASE:
 			return AlterDatabaseOwner(strVal((Value *) stmt->object), newowner);
 
+		case OBJECT_ROLE:
+			return AlterRoleOwner(strVal((Value *) stmt->object), newowner);
+
 		case OBJECT_SCHEMA:
 			return AlterSchemaOwner(strVal((Value *) stmt->object), newowner);
 
