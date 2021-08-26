076502c72a3ec0941982ab6f5047a4d28bd29329
hackorum <git@hackorum.dev>
2021-08-26 07:21:51 +0000

    Apply remove_superuser_check_from_RemoveAccessMethodById.patch

--- src/backend/commands/amcmds.c
+++ src/backend/commands/amcmds.c
@@ -124,11 +124,6 @@ RemoveAccessMethodById(Oid amOid)
 	Relation	relation;
 	HeapTuple	tup;
 
-	if (!superuser())
-		ereport(ERROR,
-				(errcode(ERRCODE_INSUFFICIENT_PRIVILEGE),
-				 errmsg("must be superuser to drop access methods")));
-
 	relation = table_open(AccessMethodRelationId, RowExclusiveLock);
 
 	tup = SearchSysCache1(AMOID, ObjectIdGetDatum(amOid));
