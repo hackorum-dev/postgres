5c5b01264f177b6f318c1d83dc6426b97852c20b
hackorum <git@hackorum.dev>
2021-09-10 18:28:17 +0000

    Apply header-ident.patch

--- src/include/utils/dynahash.h
+++ src/include/utils/dynahash.h
@@ -7,7 +7,8 @@
  * Portions Copyright (c) 1996-2021, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * src/include/utils/dynahash.h
+ * IDENTIFICATION
+ *		src/include/utils/dynahash.h
  *
  *-------------------------------------------------------------------------
  */
