15e4d49261bf213ab1f556b14f2e6bef50d12cdd
Atsushi Torikoshi <torikoshia@oss.nttdata.com>
2021-11-15 22:31:00 +0900

    Add function to log the untruncated query string and its plan for the query currently running on the backend with the specified process ID.
    Currently, we have to wait for the query execution to finish
    to check its plan. This is not so convenient when
    investigating long-running queries on production environments
    where we cannot use debuggers.
    To improve this situation, this patch adds
    pg_log_query_plan() function that requests to log the
    plan of the specified backend process.
    
    By default, only superusers are allowed to request to log the
    plans because allowing any users to issue this request at an
    unbounded rate would cause lots of log messages and which can
    lead to denial of service.
    
    On receipt of the request, at the next CHECK_FOR_INTERRUPTS(),
    the target backend logs its plan at LOG_SERVER_ONLY level, so
    that these plans will appear in the server log but not be sent
    to the client.
    
    Since some codes, tests and comments of
    pg_log_query_plan() are the same with
    pg_log_backend_memory_contexts(), this patch also refactors
    them to make them common.
    
    Reviewed-by: Bharath Rupireddy, Fujii Masao, Dilip Kumar, Masahiro Ikeda, Ekaterina Sokolova, Justin Pryzby

--- src/backend/catalog/system_functions.sql
+++ src/backend/catalog/system_functions.sql
@@ -701,6 +701,8 @@ REVOKE EXECUTE ON FUNCTION pg_ls_dir(text,boolean,boolean) FROM public;
 
 REVOKE EXECUTE ON FUNCTION pg_log_backend_memory_contexts(integer) FROM PUBLIC;
 
+REVOKE EXECUTE ON FUNCTION pg_log_query_plan(integer) FROM PUBLIC;
+
 --
 -- We also set up some things as accessible to standard roles.
 --
