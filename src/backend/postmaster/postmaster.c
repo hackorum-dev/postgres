a39cfed31fb069f06d007bc188a66603baf02284
Bharath Rupireddy <bharath.rupireddyforpostgres@gmail.com>
2021-12-03 07:56:49 +0000

    remove extra Logging_collector check before SysLogger_Start()

--- src/backend/postmaster/postmaster.c
+++ src/backend/postmaster/postmaster.c
@@ -1813,7 +1813,7 @@ ServerLoop(void)
 		}
 
 		/* If we have lost the log collector, try to start a new one */
-		if (SysLoggerPID == 0 && Logging_collector)
+		if (SysLoggerPID == 0)
 			SysLoggerPID = SysLogger_Start();
 
 		/*
