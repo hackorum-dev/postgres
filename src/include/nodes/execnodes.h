354fda678c602c5d76dee3eb760d91cb4aacc65b
Dmitrii Dolgov <9erthalion6@gmail.com>
2022-01-22 21:13:42 +0100

    Extend index skip scan with ScanLooseKey
    Index skip scan relies on the information about key prefix that needs to
    be jumped over, but it's represented in a rather limited fashion only
    via the prefix size. This approach is sufficient now, but for the sake
    of flexibility introduce a concept of ScanLooseKey to represent
    underspecified search keys. At the moment it's inspired by the idea of
    skip keys constrained in some range of keyspace, and not used in any
    way.

--- src/include/nodes/execnodes.h
+++ src/include/nodes/execnodes.h
@@ -1508,6 +1508,8 @@ typedef struct IndexScanState
  *		NumOrderByKeys	   number of OrderByKeys
  *		RuntimeKeys		   info about Skeys that must be evaluated at runtime
  *		NumRuntimeKeys	   number of RuntimeKeys
+ *		ScanLooseKeys 	   Skey structures for loose index quals
+ *		NumScanLooseKeys   number of ScanLooseKeys
  *		RuntimeKeysReady   true if runtime Skeys have been computed
  *		RuntimeContext	   expr context for evaling runtime Skeys
  *		RelationDesc	   index relation descriptor
@@ -1527,13 +1529,14 @@ typedef struct IndexOnlyScanState
 	int			ioss_NumOrderByKeys;
 	IndexRuntimeKeyInfo *ioss_RuntimeKeys;
 	int			ioss_NumRuntimeKeys;
+	struct ScanLooseKeyData *ioss_ScanLooseKeys;
+	int			ioss_NumScanLooseKeys;
 	bool		ioss_RuntimeKeysReady;
 	ExprContext *ioss_RuntimeContext;
 	Relation	ioss_RelationDesc;
 	struct IndexScanDescData *ioss_ScanDesc;
 	TupleTableSlot *ioss_TableSlot;
 	Buffer		ioss_VMBuffer;
-	int         ioss_SkipPrefixSize;
 	bool		ioss_FirstTupleEmitted;
 	Size		ioss_PscanLen;
 } IndexOnlyScanState;
