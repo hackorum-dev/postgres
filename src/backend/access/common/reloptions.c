8138316fe49be6540926e125501d25a65c3b0691
Junwang Zhao <zhjwpku@gmail.com>
2022-09-01 16:07:11 +0800

    use NoLock instead of the magic 0
    Inside add_local_<>_reloption, we should pass NoLock instead of
    the magic 0 to init_<>_reloption, which makes more sense.
    
    Signed-off-by: Junwang Zhao <zhjwpku@gmail.com>

--- src/backend/access/common/reloptions.c
+++ src/backend/access/common/reloptions.c
@@ -1021,7 +1021,7 @@ add_local_bool_reloption(local_relopts *relopts, const char *name,
 {
 	relopt_bool *newoption = init_bool_reloption(RELOPT_KIND_LOCAL,
 												 name, desc,
-												 default_val, 0);
+												 default_val, NoLock);
 
 	add_local_reloption(relopts, (relopt_gen *) newoption, offset);
 }
@@ -1124,7 +1124,7 @@ add_local_int_reloption(local_relopts *relopts, const char *name,
 {
 	relopt_int *newoption = init_int_reloption(RELOPT_KIND_LOCAL,
 											   name, desc, default_val,
-											   min_val, max_val, 0);
+											   min_val, max_val, NoLock);
 
 	add_local_reloption(relopts, (relopt_gen *) newoption, offset);
 }
@@ -1179,7 +1179,7 @@ add_local_real_reloption(local_relopts *relopts, const char *name,
 	relopt_real *newoption = init_real_reloption(RELOPT_KIND_LOCAL,
 												 name, desc,
 												 default_val, min_val,
-												 max_val, 0);
+												 max_val, NoLock);
 
 	add_local_reloption(relopts, (relopt_gen *) newoption, offset);
 }
@@ -1243,7 +1243,7 @@ add_local_enum_reloption(local_relopts *relopts, const char *name,
 	relopt_enum *newoption = init_enum_reloption(RELOPT_KIND_LOCAL,
 												 name, desc,
 												 members, default_val,
-												 detailmsg, 0);
+												 detailmsg, NoLock);
 
 	add_local_reloption(relopts, (relopt_gen *) newoption, offset);
 }
@@ -1327,7 +1327,7 @@ add_local_string_reloption(local_relopts *relopts, const char *name,
 													 name, desc,
 													 default_val,
 													 validator, filler,
-													 0);
+													 NoLock);
 
 	add_local_reloption(relopts, (relopt_gen *) newoption, offset);
 }
