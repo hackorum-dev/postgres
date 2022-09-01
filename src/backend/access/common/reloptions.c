17f481206e4e5d8a7afa0031510ef176ee901eb0
Junwang Zhao <zhjwpku@gmail.com>
2022-09-01 16:07:11 +0800

    use NoLock instead of the magic 0
    Inside add_local_<>_reloption, we should pass NoLock instead of
    the magic 0 to init_<>_reloption, which makes more sense.
    
    Signed-off-by: Junwang Zhao <zhjwpku@gmail.com>

--- src/backend/access/common/reloptions.c
+++ src/backend/access/common/reloptions.c
@@ -928,7 +928,7 @@ add_local_bool_reloption(local_relopts *relopts, const char *name,
 {
 	relopt_bool *newoption = init_bool_reloption(RELOPT_KIND_LOCAL,
 												 name, desc,
-												 default_val, 0);
+												 default_val, NoLock);
 
 	add_local_reloption(relopts, (relopt_gen *) newoption, offset);
 }
@@ -1031,7 +1031,7 @@ add_local_int_reloption(local_relopts *relopts, const char *name,
 {
 	relopt_int *newoption = init_int_reloption(RELOPT_KIND_LOCAL,
 											   name, desc, default_val,
-											   min_val, max_val, 0);
+											   min_val, max_val, NoLock);
 
 	add_local_reloption(relopts, (relopt_gen *) newoption, offset);
 }
@@ -1086,7 +1086,7 @@ add_local_real_reloption(local_relopts *relopts, const char *name,
 	relopt_real *newoption = init_real_reloption(RELOPT_KIND_LOCAL,
 												 name, desc,
 												 default_val, min_val,
-												 max_val, 0);
+												 max_val, NoLock);
 
 	add_local_reloption(relopts, (relopt_gen *) newoption, offset);
 }
@@ -1150,7 +1150,7 @@ add_local_enum_reloption(local_relopts *relopts, const char *name,
 	relopt_enum *newoption = init_enum_reloption(RELOPT_KIND_LOCAL,
 												 name, desc,
 												 members, default_val,
-												 detailmsg, 0);
+												 detailmsg, NoLock);
 
 	add_local_reloption(relopts, (relopt_gen *) newoption, offset);
 }
@@ -1234,7 +1234,7 @@ add_local_string_reloption(local_relopts *relopts, const char *name,
 													 name, desc,
 													 default_val,
 													 validator, filler,
-													 0);
+													 NoLock);
 
 	add_local_reloption(relopts, (relopt_gen *) newoption, offset);
 }
