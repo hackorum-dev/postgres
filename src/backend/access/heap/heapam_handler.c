9696cefe9f860dca430ddc2c3d296fed776408db
Mats Kindahl <mats@timescale.com>
2022-10-31 14:27:03 +0100

    Add callback for unlinking file node of table
    When dropping or truncating a table, the old file locator is unlinked
    and a new file locator created for the relation. This is, however, done
    without notifying the table access method that the unlinking is taking
    place. This becomes a problem if the table access method has an
    internal resource associated with the file locator that also needs to
    be unlinked or cleaned up.
    
    This commit add a callback to the table access method for resetting the
    file locator when it is dropped and call it to unlink the file locator
    so that the table access method and unlink the resource internally, if
    necessary. The callback can be undefined (that is, NULL) if the table
    access method do not require notification that a resource is unlinked.

--- src/backend/access/heap/heapam_handler.c
+++ src/backend/access/heap/heapam_handler.c
@@ -526,6 +526,12 @@ heapam_relation_set_new_filelocator(Relation rel,
 }
 
 static void
+heapam_relation_reset_filelocator(Relation rel)
+{
+	RelationDropStorage(rel);
+}
+
+static void
 heapam_relation_nontransactional_truncate(Relation rel)
 {
 	RelationTruncate(rel, 0);
@@ -2693,6 +2699,7 @@ static const TableAmRoutine heapam_methods = {
 	.index_delete_tuples = heap_index_delete_tuples,
 
 	.relation_set_new_filelocator = heapam_relation_set_new_filelocator,
+	.relation_reset_filelocator = heapam_relation_reset_filelocator,
 	.relation_nontransactional_truncate = heapam_relation_nontransactional_truncate,
 	.relation_copy_data = heapam_relation_copy_data,
 	.relation_copy_for_cluster = heapam_relation_copy_for_cluster,
