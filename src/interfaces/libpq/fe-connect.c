3ef3835ec50dbe66d652b9bb872707f589a1777f
Jim Jones <jim.jones@uni-muenster.de>
2022-11-21 15:05:33 +0100

    add sslmode no-clientcert

--- src/interfaces/libpq/fe-connect.c
+++ src/interfaces/libpq/fe-connect.c
@@ -1794,6 +1794,7 @@ pqConnectOptions2(PGconn *conn)
 			&& strcmp(conn->sslmode, "allow") != 0
 			&& strcmp(conn->sslmode, "prefer") != 0
 			&& strcmp(conn->sslmode, "require") != 0
+			&& strcmp(conn->sslmode, "no-clientcert") != 0
 			&& strcmp(conn->sslmode, "verify-ca") != 0
 			&& strcmp(conn->sslmode, "verify-full") != 0)
 		{
