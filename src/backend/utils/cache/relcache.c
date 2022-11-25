b5f06db84cfc58a1815f6915a2f430133712b8da
hackorum <git@hackorum.dev>
2022-11-25 15:56:13 +0000

    Apply build-desc-check-rules.patch

--- src/backend/utils/cache/relcache.c
+++ src/backend/utils/cache/relcache.c
@@ -1249,7 +1249,11 @@ retry:
 	 * extracting the relation's reloptions.
 	 */
 	if (relation->rd_rel->relhasrules)
+	{
 		RelationBuildRuleLock(relation);
+		if (relation->rd_rules == NULL)
+			relation->rd_rel->relhasrules = false;
+	}
 	else
 	{
 		relation->rd_rules = NULL;
