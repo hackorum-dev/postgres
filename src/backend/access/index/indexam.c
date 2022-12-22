hackorum-dev/postgres

commit 941aa6a6268a6a66f6895401aad6b5329111d412

Check the snapshot argument of index_beginscan and family
Passing a NULL snapshot (InvalidSnapshot) is going to work but only as long
as the index can't find any matching rows.  This can be confusing for
the extension authors, so add an explicit check for this argument.  The check
is implemented with Assert() in order to avoid overhead in release builds.

Reported-by: Sven Klemm
Discussion: https://postgr.es/m/CAJ7c6TPxitD4vbKyP-mpmC1XwyHdPPqvjLzm%2BVpB88h8LGgneQ%40mail.gmail.com
Author: Aleksander Alekseev
Reviewed-by: Pavel Borisov

src/backend/access/index/indexam.c
@@ -209,6 +209,8 @@ index_beginscan(Relation heapRelation,
 {
 	IndexScanDesc scan;
 
+	Assert(snapshot != InvalidSnapshot);
+
 	scan = index_beginscan_internal(indexRelation, nkeys, norderbys, snapshot, NULL, false);
 
 	/*
@@ -236,6 +238,8 @@ index_beginscan_bitmap(Relation indexRelation,
 					   int nkeys)
 {
 	IndexScanDesc scan;
+
+	Assert(snapshot != InvalidSnapshot);
 
 	scan = index_beginscan_internal(indexRelation, nkeys, 0, snapshot, NULL, false);
 
@@ -403,6 +407,8 @@ index_parallelscan_estimate(Relation indexRelation, Snapshot snapshot)
 {
 	Size		nbytes;
 
+	Assert(snapshot != InvalidSnapshot);
+
 	RELATION_CHECKS;
 
 	nbytes = offsetof(ParallelIndexScanDescData, ps_snapshot_data);
@@ -436,6 +442,8 @@ index_parallelscan_initialize(Relation heapRelation, Relation indexRelation,
 							  Snapshot snapshot, ParallelIndexScanDesc target)
 {
 	Size		offset;
+
+	Assert(snapshot != InvalidSnapshot);
 
 	RELATION_CHECKS;
 

commit d5ae46fbf6656fb929f1001e9ec6020232865f8a

Optimize asserts introduced in 941aa6a6268a6a66f68
Two asserts combined, one unnecessary removed.

Authors: Ted Yu <yuzhihong@gmail.com>, Pavel Borsiov
<pashkin.elfe@gmail.com>
Reviewed-by: Pavel Borisov <pashkin.elfe@gmail.com>

src/backend/access/index/indexam.c
@@ -209,8 +209,6 @@ index_beginscan(Relation heapRelation,
 {
 	IndexScanDesc scan;
 
-	Assert(snapshot != InvalidSnapshot);
-
 	scan = index_beginscan_internal(indexRelation, nkeys, norderbys, snapshot, NULL, false);
 
 	/*
@@ -239,8 +237,6 @@ index_beginscan_bitmap(Relation indexRelation,
 {
 	IndexScanDesc scan;
 
-	Assert(snapshot != InvalidSnapshot);
-
 	scan = index_beginscan_internal(indexRelation, nkeys, 0, snapshot, NULL, false);
 
 	/*
@@ -264,6 +260,8 @@ index_beginscan_internal(Relation indexRelation,
 
 	RELATION_CHECKS;
 	CHECK_REL_PROCEDURE(ambeginscan);
+
+	Assert(snapshot != InvalidSnapshot);
 
 	if (!(indexRelation->rd_indam->ampredlocks))
 		PredicateLockRelation(indexRelation, snapshot);
@@ -406,8 +404,6 @@ Size
 index_parallelscan_estimate(Relation indexRelation, Snapshot snapshot)
 {
 	Size		nbytes;
-
-	Assert(snapshot != InvalidSnapshot);
 
 	RELATION_CHECKS;
 