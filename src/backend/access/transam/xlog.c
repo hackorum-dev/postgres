10df1d5f389a310c61de088ffab782cfd29555cc
Bharath Rupireddy <bharath.rupireddyforpostgres@gmail.com>
2024-04-13 08:30:59 +0000

    Remove unnecessary seg no calculation after check for slot invalidation.

--- src/backend/access/transam/xlog.c
+++ src/backend/access/transam/xlog.c
@@ -7880,7 +7880,6 @@ CreateCheckPoint(int flags)
 		 * Some slots have been invalidated; recalculate the old-segment
 		 * horizon, starting again from RedoRecPtr.
 		 */
-		XLByteToSeg(RedoRecPtr, _logSegNo, wal_segment_size);
 		KeepLogSeg(recptr, &_logSegNo);
 	}
 	_logSegNo--;
@@ -8353,7 +8352,6 @@ CreateRestartPoint(int flags)
 		 * Some slots have been invalidated; recalculate the old-segment
 		 * horizon, starting again from RedoRecPtr.
 		 */
-		XLByteToSeg(RedoRecPtr, _logSegNo, wal_segment_size);
 		KeepLogSeg(endptr, &_logSegNo);
 	}
 	_logSegNo--;
