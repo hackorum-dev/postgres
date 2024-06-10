2f97bf4a5143dc6e98ea693d605059b2b46d78ee
Ilia Evdokimov <ilya.evdokimov@tantorlabs.ru>
2024-06-10 16:39:14 +0300

    After concatenation two lists where the second one is from list_copy_tail do not free it

--- src/backend/parser/analyze.c
+++ src/backend/parser/analyze.c
@@ -2304,6 +2304,8 @@ transformSetOperationStmt(ParseState *pstate, SelectStmt *stmt)
 										NULL,
 										false);
 
+   list_free(targetnames);
+
 	sv_namespace = pstate->p_namespace;
 	pstate->p_namespace = NIL;
 
