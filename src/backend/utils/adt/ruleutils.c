37757a8c289849cccbb132d2fcdc0bbce6b3f269
hackorum <git@hackorum.dev>
2024-08-19 11:23:51 +0000

    Apply minor_fix.diff

--- src/backend/utils/adt/ruleutils.c
+++ src/backend/utils/adt/ruleutils.c
@@ -8296,7 +8296,8 @@ get_name_for_var_field(Var *var, int fieldno,
 					{
 						char	   *dummy_name = palloc(32);
 
-						Assert(dpns->plan && IsA(dpns->plan, Result));
+						Assert(dpns->plan && (IsA(dpns->plan, Result) ||
+											  IsA(dpns->plan, CustomScan)));
 						snprintf(dummy_name, 32, "f%d", fieldno);
 						return dummy_name;
 					}
@@ -8427,7 +8428,8 @@ get_name_for_var_field(Var *var, int fieldno,
 					{
 						char	   *dummy_name = palloc(32);
 
-						Assert(dpns->plan && IsA(dpns->plan, Result));
+						Assert(dpns->plan && (IsA(dpns->plan, Result) ||
+											  IsA(dpns->plan, CustomScan)));
 						snprintf(dummy_name, 32, "f%d", fieldno);
 						return dummy_name;
 					}
