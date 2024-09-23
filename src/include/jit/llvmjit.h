5a390fb8ed846e89cc0ff3ffb2ee795a179a7bc7
Dmitrii Dolgov <9erthalion6@gmail.com>
2024-09-23 19:08:52 +0200

    Add jit provider's version into the pgsql_version
    If jit provider is available and initialized, add its version string
    into the pgsql_version output. This should help to get more relevant
    information in bug reports about JIT.

--- src/include/jit/llvmjit.h
+++ src/include/jit/llvmjit.h
@@ -145,6 +145,8 @@ extern LLVMTypeRef LLVMGetFunctionType(LLVMValueRef r);
 extern LLVMOrcObjectLayerRef LLVMOrcCreateRTDyldObjectLinkingLayerWithSafeSectionMemoryManager(LLVMOrcExecutionSessionRef ES);
 #endif
 
+extern const char* llvm_version(bool *available);
+
 #ifdef __cplusplus
 } /* extern "C" */
 #endif
