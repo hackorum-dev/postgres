d0a723b1c413b7d9196a8b8c17b560ca375f824b
Daniel Gustafsson <dgustafsson@postgresql.org>
2025-02-06 22:32:55 +0100

    Fix parameter description in comment
    Commit 19d8e23 changed the datatype of the update_index parameter but
    accidentally missed updating the documentation comment.
    
    Reported-by: Sergei Kornilov <sk@zsrv.org>
    Discussion: https://postgr.es/m/1097041738868417@zxq47dct3nfd6lzq.sas.yp-c.yandex.net

--- src/include/access/tableam.h
+++ src/include/access/tableam.h
@@ -1520,8 +1520,8 @@ table_tuple_delete(Relation rel, ItemPointer tid, CommandId cid,
  * Output parameters:
  *	tmfd - filled in failure cases (see below)
  *	lockmode - filled with lock mode acquired on tuple
- *  update_indexes - in success cases this is set to true if new index entries
- *		are required for this tuple
+ *  update_indexes - in successful cases this indicates the index types
+ *		which require new index entries for this tuple
  *
  * Normal, successful return value is TM_Ok, which means we did actually
  * update it.  Failure return codes are TM_SelfModified, TM_Updated, and
