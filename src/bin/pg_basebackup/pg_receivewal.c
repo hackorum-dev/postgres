3bddfc9e4d3a774d7d319dcfe2eff552f5396fcd
hackorum <git@hackorum.dev>
2025-02-11 11:49:06 +0000

    Apply fix-resource-leak-pg_receivewal.patch

--- src/bin/pg_basebackup/pg_receivewal.c
+++ src/bin/pg_basebackup/pg_receivewal.c
@@ -548,6 +548,7 @@ StreamLog(void)
 			if (!GetSlotInformation(conn, replication_slot, &stream.startpos,
 									&stream.timeline))
 			{
+				pg_free(sysidentifier);
 				/* Error is logged by GetSlotInformation() */
 				return;
 			}
