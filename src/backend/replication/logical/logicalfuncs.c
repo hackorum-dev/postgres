a48d1f953a243daa9c91772753999b262b33dc1d
Hayato Kuroda <kuroda.hayato@fujitsu.com>
2025-02-19 11:37:26 +0900

    Prohibit slot manipulation while in single-user mode
    Replication-related commands are rarely used in single-user mode and have been
    broken for years. This commit prohibits calling slot manipulation SQL functions
    to avoid additional risk of failures. One exception is pg_drop_replication_slot.
    It is still allowed because users may want to clean up their mistakes in the mode.

--- src/backend/replication/logical/logicalfuncs.c
+++ src/backend/replication/logical/logicalfuncs.c
@@ -113,6 +113,9 @@ pg_logical_slot_get_changes_guts(FunctionCallInfo fcinfo, bool confirm, bool bin
 	List	   *options = NIL;
 	DecodingOutputState *p;
 
+	/* Slot manipulation is not allowed in single-user mode */
+	CheckSlotIsInSingleUserMode();
+
 	CheckSlotPermissions();
 
 	CheckLogicalDecodingRequirements(false);
