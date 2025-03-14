22d847f2955923431101a55c453e771d1378ae03
Andrei V. Lepikhov <lepihov@gmail.com>
2025-03-14 12:51:42 +0100

    Enhance partition pruning for an array parameter.
    It is designed to prune partitions in case when incoming clause looks like
    the following: 'partkey = ANY($1)'.
    
    It seems quite a common case when the array is a parameter of a function.
    
    Although the code is covered by tests the code should be carefully reviewed and
    tested.

--- src/backend/partitioning/partprune.c
+++ src/backend/partitioning/partprune.c
@@ -2190,6 +2190,9 @@ match_clause_to_partition_key(GeneratePruningStepsContext *context,
 		List	   *elem_exprs,
 				   *elem_clauses;
 		ListCell   *lc1;
+		int			strategy;
+		Oid			lefttype,
+					righttype;
 
 		leftop = (Expr *) strip_noop_phvs((Node *) leftop);
 		while (IsA(leftop, RelabelType))
@@ -2218,10 +2221,6 @@ match_clause_to_partition_key(GeneratePruningStepsContext *context,
 			negator = get_negator(saop_op);
 			if (OidIsValid(negator) && op_in_opfamily(negator, partopfamily))
 			{
-				int			strategy;
-				Oid			lefttype,
-							righttype;
-
 				get_op_opfamily_properties(negator, partopfamily,
 										   false, &strategy,
 										   &lefttype, &righttype);
@@ -2230,6 +2229,12 @@ match_clause_to_partition_key(GeneratePruningStepsContext *context,
 			}
 			else
 				return PARTCLAUSE_NOMATCH;	/* no useful negator */
+		}
+		else
+		{
+			get_op_opfamily_properties(saop_op, partopfamily, false,
+									   &strategy, &lefttype,
+									   &righttype);
 		}
 
 		/*
@@ -2376,6 +2381,64 @@ match_clause_to_partition_key(GeneratePruningStepsContext *context,
 			 * Otherwise, we can just use the list of element values.
 			 */
 			elem_exprs = arrexpr->elements;
+		}
+		else if (IsA(rightop, Param))
+		{
+			Oid				cmpfn;
+			PartClauseInfo *partclause;
+
+			if (righttype == part_scheme->partopcintype[partkeyidx])
+				cmpfn = part_scheme->partsupfunc[partkeyidx].fn_oid;
+			else
+			{
+				switch (part_scheme->strategy)
+				{
+					/*
+					 * For range and list partitioning, we need the ordering
+					 * procedure with lefttype being the partition key's type,
+					 * and righttype the clause's operator's right type.
+					 */
+				case PARTITION_STRATEGY_LIST:
+				case PARTITION_STRATEGY_RANGE:
+					cmpfn =
+						get_opfamily_proc(part_scheme->partopfamily[partkeyidx],
+										  part_scheme->partopcintype[partkeyidx],
+										  righttype, BTORDER_PROC);
+					break;
+
+					/*
+					 * For hash partitioning, we need the hashing procedure
+					 * for the clause's type.
+					 */
+				case PARTITION_STRATEGY_HASH:
+					cmpfn =
+						get_opfamily_proc(part_scheme->partopfamily[partkeyidx],
+										  righttype, righttype,
+										  HASHEXTENDED_PROC);
+					break;
+
+				default:
+					elog(ERROR, "invalid partition strategy: %c",
+						 part_scheme->strategy);
+					cmpfn = InvalidOid; /* keep compiler quiet */
+					break;
+				}
+
+				if (!OidIsValid(cmpfn))
+					return PARTCLAUSE_NOMATCH;
+			}
+
+			partclause = (PartClauseInfo *) palloc(sizeof(PartClauseInfo));
+			partclause->keyno = partkeyidx;
+			partclause->opno = saop_op;
+			partclause->op_is_ne = false;
+			partclause->op_strategy = strategy;
+			partclause->expr = rightop;
+			partclause->cmpfn = cmpfn;
+
+			*pc = partclause;
+
+			return PARTCLAUSE_MATCH_CLAUSE;
 		}
 		else
 		{
