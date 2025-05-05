b1e8c339e38cd31acd4823465764c98a117f18f5
Jacob Champion <jacob.champion@enterprisedb.com>
2025-05-05 14:19:10 -0700

    Fix connhost type during non-blocking cancellation
    PQcancelCreate() creates a deep copy of the connection's pg_conn_host,
    but it missed the type field, which ended up initialized to
    CHT_HOST_NAME. The type communicates which pointers in the struct are
    valid, so if a connection used hostaddr without a host name (which would
    normally result in a type of CHT_HOST_ADDRESS), the client would later
    segfault in emitHostIdentityInfo().
    
    Backpatch to 17, where the new API was introduced.
    
    Reviewed-by: TODO
    Backpatch-through: 17

--- src/interfaces/libpq/fe-cancel.c
+++ src/interfaces/libpq/fe-cancel.c
@@ -137,6 +137,8 @@ PQcancelCreate(PGconn *conn)
 		goto oom_error;
 
 	originalHost = conn->connhost[conn->whichhost];
+	cancelConn->connhost[0].type = originalHost.type;
+
 	if (originalHost.host)
 	{
 		cancelConn->connhost[0].host = strdup(originalHost.host);
