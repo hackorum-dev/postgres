13714074f67fbfbc4cd931bc3bd368c3d6cdd336
Tomas Vondra <tomas@vondra.me>
2025-07-22 20:56:27 +0200

    PoC: introduce CustomJoin as separate from CustomScan
    Introduce a new CustomJoin node, intended for out-of-core join nodes.
    The CustomScan is supposed to allow that, but there's a couple pieces
    that make it inconvenient. The targetlist uses INDEX_VAR, so it's not
    clear which relation the Vars come from, etc.
    
    So this adds a new node, descended from Join (not Scan), with all the
    processing specific to joins (in setrefs etc.).
    
    Experimental. Does are not updated, some bits may be irrelevant for
    joins, etc.

--- src/backend/nodes/nodeFuncs.c
+++ src/backend/nodes/nodeFuncs.c
@@ -4799,6 +4799,13 @@ planstate_tree_walker_impl(PlanState *planstate,
 					return true;
 			}
 			break;
+		case T_CustomJoin:
+			foreach(lc, ((CustomJoinState *) planstate)->custom_ps)
+			{
+				if (PSWALK(lfirst(lc)))
+					return true;
+			}
+			break;
 		default:
 			break;
 	}
