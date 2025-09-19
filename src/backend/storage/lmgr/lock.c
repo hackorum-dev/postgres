2831d320122e902d9440d957e0571c82df35006e
Aleksander Alekseev <aleksander@tigerdata.com>
2025-09-19 13:06:35 +0300

    Free memory allocated by waitonlock_error_callback()

--- src/backend/storage/lmgr/lock.c
+++ src/backend/storage/lmgr/lock.c
@@ -2038,6 +2038,8 @@ waitonlock_error_callback(void *arg)
 	errcontext("waiting for %s on %s",
 			   GetLockmodeName(tag->locktag_lockmethodid, mode),
 			   locktagbuf.data);
+
+	pfree(locktagbuf.data);
 }
 
 /*
