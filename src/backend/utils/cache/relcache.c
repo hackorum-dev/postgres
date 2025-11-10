cf1dc45de6737f8aeb45485edb11cf52af84a59b
Jingtang Zhang <mrdrivingduck@gmail.com>
2025-11-11 00:04:24 +0800

    Remove redundant initialization of smgr pointer for relcache
    The smgr object pointer of a relcache is commonly initialized after the
    RelationData is palloc'ed. This commit removes a redundant assign.

--- src/backend/utils/cache/relcache.c
+++ src/backend/utils/cache/relcache.c
@@ -1276,9 +1276,6 @@ retry:
 	 */
 	RelationInitPhysicalAddr(relation);
 
-	/* make sure relation is marked as having no open file yet */
-	relation->rd_smgr = NULL;
-
 	/*
 	 * now we can free the memory allocated for pg_class_tuple
 	 */
