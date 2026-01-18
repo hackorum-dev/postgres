f89ab63f58df53655b160bf429759e6d472bf866
reshke <reshke@double.cloud>
2026-01-18 19:50:07 +0000

    Add bt_page_opaque function.

--- contrib/pageinspect/pageinspect--1.13--1.14.sql
+++ contrib/pageinspect/pageinspect--1.13--1.14.sql
@@ -1,5 +1,9 @@
 
 
+--
+-- bt_page_items()
+--
+
 DROP FUNCTION bt_page_items(text, int8);
 CREATE FUNCTION bt_page_items(IN relname text, IN blkno int8,
     IN pretty_print boolean,
@@ -14,4 +18,18 @@ CREATE FUNCTION bt_page_items(IN relname text, IN blkno int8,
     OUT tids tid[])
 RETURNS SETOF record
 AS 'MODULE_PATHNAME', 'bt_page_items_1_9'
+LANGUAGE C STRICT PARALLEL SAFE;
+
+
+--
+-- bt_page_opaque()
+--
+CREATE FUNCTION bt_page_opaque(IN page bytea,
+    OUT lsn pg_lsn,
+    OUT btpo_prev INT,
+    OUT btpo_next INT,
+    OUT btpo_level INT,
+    OUT flags text[],
+    OUT btpo_cycleid SMALLINT)
+AS 'MODULE_PATHNAME', 'bt_page_opaque'
 LANGUAGE C STRICT PARALLEL SAFE;
