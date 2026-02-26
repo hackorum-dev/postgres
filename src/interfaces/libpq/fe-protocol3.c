096f6e7e23f8c7eafbb1bae2f1d596f041b668d4
zourenli <398740848@qq.com>
2026-02-26 10:54:23 +0800

    libpq: Fix incorrect message type in getBackendKeyData() comment
    The getBackendKeyData function comment incorrectly stated that the
    BackendKeyData message type is 'v', when in fact it is 'K' as defined
    in protocol.h. This fix corrects the comment to match the actual
    message type used in the protocol.

--- src/interfaces/libpq/fe-protocol3.c
+++ src/interfaces/libpq/fe-protocol3.c
@@ -1579,7 +1579,7 @@ getParameterStatus(PGconn *conn)
  * parseInput subroutine to read a BackendKeyData message.
  * Entry: 'K' message type and length have already been consumed.
  * Exit: returns 0 if successfully consumed message.
- *		 returns EOF if not enough data.
+ *              returns EOF if not enough data.
  */
 static int
 getBackendKeyData(PGconn *conn, int msgLength)
