9689babc0941cd6c4f79541315f58337d02fdb57
hackorum <git@hackorum.dev>
2026-03-09 13:16:20 +0000

    Apply avoid-multiple-calls-to-memcpy-genam.patch

--- src/backend/access/index/genam.c
+++ src/backend/access/index/genam.c
@@ -447,12 +447,12 @@ systable_beginscan(Relation heapRelation,
 
 		idxkey = palloc_array(ScanKeyData, nkeys);
 
+		memcpy(idxkey, key, nkeys * sizeof(ScanKeyData));
+
 		/* Convert attribute numbers to be index column numbers. */
 		for (i = 0; i < nkeys; i++)
 		{
 			int			j;
-
-			memcpy(&idxkey[i], &key[i], sizeof(ScanKeyData));
 
 			for (j = 0; j < IndexRelationGetNumberOfAttributes(irel); j++)
 			{
@@ -702,12 +702,12 @@ systable_beginscan_ordered(Relation heapRelation,
 
 	idxkey = palloc_array(ScanKeyData, nkeys);
 
+	memcpy(idxkey, key, nkeys * sizeof(ScanKeyData));
+
 	/* Convert attribute numbers to be index column numbers. */
 	for (i = 0; i < nkeys; i++)
 	{
 		int			j;
-
-		memcpy(&idxkey[i], &key[i], sizeof(ScanKeyData));
 
 		for (j = 0; j < IndexRelationGetNumberOfAttributes(indexRelation); j++)
 		{
