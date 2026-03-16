e623cfc3e2469c3ea99bc46db12e223c294151f3
Atsushi Torikoshi <torikoshia@oss.nttdata.com>
2026-03-16 23:58:06 +0900

    Add function to log the plan of the currently running
    Currently, we have to wait for the query execution to finish
    to know its plan either using EXPLAIN ANALYZE or auto_explain.
    This is not so convenient, for example when investigating
    long-running queries on production environments.
    To improve this situation, this patch adds pg_log_query_plan()
    function that requests to log the plan of the currently
    executing query.
    
    On receipt of the request, ExecProcNodes of the current plan
    node and its subsidiary nodes are wrapped with
    ExecProcNodeFirst, which implelements logging query plan.
    When executor executes the one of the wrapped nodes, the
    query plan is logged.
    
    Our initial idea was to send a signal to the target backend
    process, which invokes EXPLAIN logic at the next
    CHECK_FOR_INTERRUPTS() call. However, we realized during
    prototyping that EXPLAIN is complex and may not be safely
    executed at arbitrary interrupt points.
    
    By default, only superusers are allowed to request to log the
    plans because allowing any users to issue this request at an
    unbounded rate would cause lots of log messages and which can
    lead to denial of service.
    
    Todo: Consider removing the PID from the log output of
    pg_log_backend_memory_contexts() and pg_log_query_plan().
    For detail, see the discussion:
    https://www.postgresql.org/message-id/CA%2BTgmoY81r7npTS34N_5MLA_u6ghfor5HoSaar53veXUYu1OxQ%40mail.gmail.com

--- contrib/auto_explain/auto_explain.c
+++ contrib/auto_explain/auto_explain.c
@@ -15,6 +15,7 @@
 #include <limits.h>
 
 #include "access/parallel.h"
+#include "commands/dynamic_explain.h"
 #include "commands/explain.h"
 #include "commands/explain_format.h"
 #include "commands/explain_state.h"
@@ -404,26 +405,9 @@ explain_ExecutorEnd(QueryDesc *queryDesc)
 			es->format = auto_explain_log_format;
 			es->settings = auto_explain_log_settings;
 
-			ExplainBeginOutput(es);
-			ExplainQueryText(es, queryDesc);
-			ExplainQueryParameters(es, queryDesc->params, auto_explain_log_parameter_max_length);
-			ExplainPrintPlan(es, queryDesc);
-			if (es->analyze && auto_explain_log_triggers)
-				ExplainPrintTriggers(es, queryDesc);
-			if (es->costs)
-				ExplainPrintJITSummary(es, queryDesc);
-			ExplainEndOutput(es);
-
-			/* Remove last line break */
-			if (es->str->len > 0 && es->str->data[es->str->len - 1] == '\n')
-				es->str->data[--es->str->len] = '\0';
-
-			/* Fix JSON to output an object */
-			if (auto_explain_log_format == EXPLAIN_FORMAT_JSON)
-			{
-				es->str->data[0] = '{';
-				es->str->data[es->str->len - 1] = '}';
-			}
+			ExplainStringAssemble(es, queryDesc, auto_explain_log_format,
+								  auto_explain_log_triggers,
+								  auto_explain_log_parameter_max_length);
 
 			/*
 			 * Note: we rely on the existing logging of context or
--- src/include/commands/explain.h
+++ src/include/commands/explain.h
@@ -70,8 +70,10 @@ extern void ExplainOnePlan(PlannedStmt *plannedstmt, IntoClause *into,
 						   const BufferUsage *bufusage,
 						   const MemoryContextCounters *mem_counters);
 
-extern void ExplainPrintPlan(ExplainState *es, QueryDesc *queryDesc);
-extern void ExplainPrintTriggers(ExplainState *es,
+extern void ExplainPrintPlan(struct ExplainState *es, QueryDesc *queryDesc);
+extern void ExplainPrintTriggers(struct ExplainState *es, QueryDesc *queryDesc);
+extern void ExplainPrintPlan(struct ExplainState *es, QueryDesc *queryDesc);
+extern void ExplainPrintTriggers(struct ExplainState *es,
 								 QueryDesc *queryDesc);
 
 extern void ExplainPrintJITSummary(ExplainState *es,
@@ -80,5 +82,8 @@ extern void ExplainPrintJITSummary(ExplainState *es,
 extern void ExplainQueryText(ExplainState *es, QueryDesc *queryDesc);
 extern void ExplainQueryParameters(ExplainState *es,
 								   ParamListInfo params, int maxlen);
+extern void ExplainStringAssemble(struct ExplainState *es, QueryDesc *queryDesc,
+								  int logFormat, bool logTriggers,
+								  int logParameterMaxLength);
 
 #endif							/* EXPLAIN_H */
