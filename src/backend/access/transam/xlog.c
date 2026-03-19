03627c53ffb0b566335f8cea0816afb3a6a94ba4
David Steele <david@pgmasters.net>
2026-03-19 02:50:51 +0000

    Add pg_control flag to prevent recovery without backup_label.
    Harden recovery by adding a flag to pg_control to indicate that backup_label is
    required. This prevents the user from deleting backup_label resulting in an
    inconsistent recovery.
    
    Another advantage is that the copy of pg_control used by pg_basebackup is
    guaranteed not to be torn.
    
    This functionality is limited to pg_basebackup (or any software comfortable
    with modifying pg_control).
    
    Control and catalog version bumps are required.

--- src/backend/access/transam/xlog.c
+++ src/backend/access/transam/xlog.c
@@ -9593,6 +9593,42 @@ do_pg_abort_backup(int code, Datum arg)
 }
 
 /*
+ * Create a consistent copy of control data to be used for backup and update it
+ * to require a backup label for recovery. Also recalculate the CRC.
+ */
+void
+backup_control_file(uint8_t *controlFile)
+{
+	ControlFileData *controlData = ((ControlFileData *)controlFile);
+
+	memset(controlFile, 0, PG_CONTROL_FILE_SIZE);
+
+	LWLockAcquire(ControlFileLock, LW_SHARED);
+	memcpy(controlFile, ControlFile, sizeof(ControlFileData));
+
+#ifdef USE_ASSERT_CHECKING
+	/*
+	 * Verify that the contents of pg_control are the same in memory as on disk
+	 */
+	{
+		bool crc_ok;
+		ControlFileData *dataDisk = get_controlfile(DataDir, &crc_ok);
+
+		Assert(crc_ok &&
+			   memcmp(dataDisk, controlFile, sizeof(ControlFileData)) == 0);
+	}
+#endif
+
+	LWLockRelease(ControlFileLock);
+
+	controlData->backupLabelRequired = true;
+
+	INIT_CRC32C(controlData->crc);
+	COMP_CRC32C(controlData->crc, controlFile, offsetof(ControlFileData, crc));
+	FIN_CRC32C(controlData->crc);
+}
+
+/*
  * Register a handler that will warn about unterminated backups at end of
  * session, unless this has already been done.
  */
