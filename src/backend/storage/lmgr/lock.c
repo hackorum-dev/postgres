c4a0c572786504f6558d9d546d35529aa90f4d5b
Daniil Davidov <d.davydov@postgrespro.ru>
2026-03-30 13:24:03 +0700

    Get rid of redundant calculations

--- src/backend/storage/lmgr/lock.c
+++ src/backend/storage/lmgr/lock.c
@@ -1206,7 +1206,7 @@ LockAcquireExtended(const LOCKTAG *locktag,
 			 * logLockFailure = true and lock acquisition fails with dontWait
 			 * = true
 			 */
-			if (logLockFailure)
+			if (logLockFailure && message_level_is_interesting(LOG))
 			{
 				StringInfoData buf,
 							lock_waiters_sbuf,
