56e14b234996f1712f2c5a04be15859fa6e954df
hackorum <git@hackorum.dev>
2026-05-03 20:17:14 +0000

    Apply v5_0001-Add-archive_mode-shared-for-coordinated-WAL-archiving.patch

--- src/backend/postmaster/postmaster.c
+++ src/backend/postmaster/postmaster.c
@@ -3383,7 +3383,9 @@ LaunchMissingBackgroundProcesses(void)
 	 */
 	if (PgArchPMChild == NULL &&
 		((XLogArchivingActive() && pmState == PM_RUN) ||
-		 (XLogArchivingAlways() && (pmState == PM_RECOVERY || pmState == PM_HOT_STANDBY))) &&
+		 (XLogArchivingAlways() && (pmState == PM_RECOVERY || pmState == PM_HOT_STANDBY)) ||
+		 (XLogArchivingShared())
+		) &&
 		PgArchCanRestart())
 		PgArchPMChild = StartChildProcess(B_ARCHIVER);
 
