191ee351765a5d2c84d884d204ff2e20e08abc7a
hackorum <git@hackorum.dev>
2026-05-28 22:11:32 +0000

    Apply v6-starjoin-fastpath-planning.patch

--- src/backend/optimizer/path/costsize.c
+++ src/backend/optimizer/path/costsize.c
@@ -165,6 +165,7 @@ bool		enable_parallel_hash = true;
 bool		enable_partition_pruning = true;
 bool		enable_presorted_aggregate = true;
 bool		enable_async_append = true;
+bool		enable_starjoin_ordering = true;
 
 typedef struct
 {
