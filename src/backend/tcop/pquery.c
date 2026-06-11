792aea67cb56ed9a997c1a61a04bfd0241bb0907
Ewan Young <kdbase.hack@gmail.com>
2026-06-12 00:42:11 +0800

    Fix some typos in code comments
    Fix three misspellings in comments, with no change to code:
    
      - float.c: "becase" -> "because"
      - ts_type.h: "agrs" -> "args"
      - pquery.c: "Querys" -> "Queries"

--- src/backend/tcop/pquery.c
+++ src/backend/tcop/pquery.c
@@ -197,7 +197,7 @@ ProcessQuery(PlannedStmt *plan,
  * ChoosePortalStrategy
  *		Select portal execution strategy given the intended statement list.
  *
- * The list elements can be Querys or PlannedStmts.
+ * The list elements can be Queries or PlannedStmts.
  * That's more general than portals need, but plancache.c uses this too.
  *
  * See the comments in portal.h.
