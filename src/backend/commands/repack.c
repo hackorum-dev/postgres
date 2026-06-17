1f4b00a33c82290725ea14b116c4d2b73c53b465
Dean Rasheed <dean.a.rasheed@gmail.com>
2026-06-17 10:04:41 +0100

    Support local vacuuming of global temporary tables.
    The per-table VACUUM results (relfrozenxid and relminmxid) are stored
    in memory for each global temporary table, instead of writing them to
    pg_class. The corresponding pg_class fields are kept invalid (zero).
    
    Additionally, each session stores its minimum relfrozenxid and
    relminmxid values over all in-use global temporary tables as
    tempfrozenxid and tempminmxid in its PGPROC struct, allowing other
    sessions to advance datfrozenxid and datminmxid, taking into account
    these minimum frozen XIDs from other sessions. This allows VACUUM to
    keep advancing datfrozenxid and datminmxid as long as each session
    that uses global temporary tables runs VACUUM from time to time.
    
    To help administrators monitor this, each backend's tempfrozenxid and
    tempminmxid values are accessible via the pg_stat_activity view.

--- src/backend/commands/repack.c
+++ src/backend/commands/repack.c
@@ -268,6 +268,7 @@ ExecRepack(ParseState *pstate, RepackStmt *stmt, bool isTopLevel)
 	bool		verbose = false;
 	bool		analyze = false;
 	bool		concurrently = false;
+	bool		have_gtrs = false;
 
 	/* Parse option list */
 	foreach_node(DefElem, opt, stmt->params)
@@ -449,6 +450,9 @@ ExecRepack(ParseState *pstate, RepackStmt *stmt, bool isTopLevel)
 			continue;
 		}
 
+		if (RELATION_IS_GLOBAL_TEMP(rel))
+			have_gtrs = true;
+
 		/* functions in indexes may want a snapshot set */
 		PushActiveSnapshot(GetTransactionSnapshot());
 
@@ -462,6 +466,13 @@ ExecRepack(ParseState *pstate, RepackStmt *stmt, bool isTopLevel)
 
 	/* Start a new transaction for the cleanup work. */
 	StartTransactionCommand();
+
+	/*
+	 * Schedule tempfrozenxid and tempminmxid to be updated on commit, if we
+	 * processed any global temporary relations.
+	 */
+	if (have_gtrs)
+		UpdateTempFrozenXids(false);
 
 	/* Clean up working storage */
 	MemoryContextDelete(repack_context);
@@ -1850,8 +1861,8 @@ swap_relation_files(Oid r1, Oid r2, bool target_is_pg_class,
 	{
 		Assert(!TransactionIdIsValid(frozenXid) ||
 			   TransactionIdIsNormal(frozenXid));
-		relform1->relfrozenxid = frozenXid;
-		relform1->relminmxid = cutoffMulti;
+		SetEffective_relfrozenxid(relform1, gtr_info1, frozenXid, NULL, NULL);
+		SetEffective_relminmxid(relform1, gtr_info1, cutoffMulti, NULL, NULL);
 	}
 
 	/* swap size statistics too, since new rel has freshly-updated stats */
@@ -2660,6 +2671,7 @@ process_single_relation(RepackStmt *stmt, LOCKMODE lockmode, bool isTopLevel,
 	else
 	{
 		Oid			indexOid = InvalidOid;
+		bool		is_gtr = RELATION_IS_GLOBAL_TEMP(rel);
 
 		indexOid = determine_clustered_index(rel, stmt->usingindex,
 											 stmt->indexname);
@@ -2691,6 +2703,13 @@ process_single_relation(RepackStmt *stmt, LOCKMODE lockmode, bool isTopLevel,
 			PopActiveSnapshot();
 			CommandCounterIncrement();
 		}
+
+		/*
+		 * Schedule tempfrozenxid and tempminmxid to be updated on commit, if
+		 * rel was a global temporary relation.
+		 */
+		if (is_gtr)
+			UpdateTempFrozenXids(false);
 
 		return NULL;
 	}
