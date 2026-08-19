5f957db6f38a15460bd36fb18d5cda8e0f35ee79
Tender Wang <tndrwang@gmail.com>
2026-08-19 14:35:38 +0800

    Remove unused arguments from split_selfjoin_quals()
    The root, from, and to arguments of split_selfjoin_quals() are not used
    by the function.  In particular, from and to are also inconsistent with
    the remove/keep terminology used by the surrounding self-join
    elimination code.
    
    Remove these unused arguments to simplify the function interface.

--- src/backend/optimizer/plan/analyzejoins.c
+++ src/backend/optimizer/plan/analyzejoins.c
@@ -2226,8 +2226,8 @@ remove_self_join_rel(PlannerInfo *root, PlanRowMark *kmark, PlanRowMark *rmark,
  * itself.
  */
 static void
-split_selfjoin_quals(PlannerInfo *root, List *joinquals, List **selfjoinquals,
-					 List **otherjoinquals, int from, int to)
+split_selfjoin_quals(List *joinquals, List **selfjoinquals,
+					 List **otherjoinquals)
 {
 	List	   *sjoinquals = NIL;
 	List	   *ojoinquals = NIL;
@@ -2462,8 +2462,7 @@ remove_self_joins_one_group(PlannerInfo *root, Relids relids)
 			 * other quals. e.g., "x = x" goes to selfjoinquals and "a = b" to
 			 * otherjoinquals.
 			 */
-			split_selfjoin_quals(root, restrictlist, &selfjoinquals,
-								 &otherjoinquals, rrel->relid, krel->relid);
+			split_selfjoin_quals(restrictlist, &selfjoinquals, &otherjoinquals);
 
 			Assert(list_length(restrictlist) ==
 				   (list_length(selfjoinquals) + list_length(otherjoinquals)));
