6f45b84918c86b5eaf3949c790e30eac120f5582
Alexandre Felipe <o.alexandre.felipe@gmail.com>
2026-08-26 08:29:48 +0100

    Multi-processing benchmark
    Improve the previous benchmark by using multiple processe
    and adding sub groups in each experiment, separating e.g.
    when a lock waited or not.

--- src/test/modules/microbench/microbench--1.0.sql
+++ src/test/modules/microbench/microbench--1.0.sql
@@ -81,3 +81,13 @@ AS $$
   GROUP BY s.op, s."group"
   ORDER BY min(s.id), s."group" NULLS FIRST;
 $$;
+CREATE FUNCTION bench_lwlock(
+	IN n_parallel int4 DEFAULT 1,
+	IN rounds int8 DEFAULT 1,
+	IN iterations int8 DEFAULT 128
+)
+RETURNS SETOF microbench_sample
+AS 'MODULE_PATHNAME', 'bench_lwlock'
+LANGUAGE C;
+
+REVOKE ALL ON FUNCTION bench_lwlock(int4, int8, int8) FROM PUBLIC;
