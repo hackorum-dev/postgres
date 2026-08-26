dd1efade2da3e8abe89b21cbe52bde5a8597349c
Alexandre Felipe <o.alexandre.felipe@gmail.com>
2026-08-26 08:29:48 +0100

    Multi-processing benchmark
    Improve the previous benchmark by using multiple processe
    and adding sub groups in each experiment, separating e.g.
    when a lock waited or not.

--- src/test/modules/microbench/bufmap/install.sql
+++ src/test/modules/microbench/bufmap/install.sql
@@ -1,10 +1,10 @@
-CREATE FUNCTION bench_lwlock(
+CREATE FUNCTION bench_bufmap(
 	IN n_parallel int4 DEFAULT 1,
 	IN rounds int8 DEFAULT 1,
 	IN iterations int8 DEFAULT 128
 )
 RETURNS SETOF microbench_sample
-AS 'MODULE_PATHNAME', 'bench_lwlock'
+AS 'MODULE_PATHNAME', 'bench_bufmap'
 LANGUAGE C;
 
-REVOKE ALL ON FUNCTION bench_lwlock(int4, int8, int8) FROM PUBLIC;
+REVOKE ALL ON FUNCTION bench_bufmap(int4, int8, int8) FROM PUBLIC;
