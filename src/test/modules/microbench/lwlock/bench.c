db97aa1ac34d41ed96f31668eec4dddc8ca25ab8
Alexandre Felipe <o.alexandre.felipe@gmail.com>
2026-08-26 08:29:48 +0100

    Multi-processing benchmark
    Improve the previous benchmark by using multiple processe
    and adding sub groups in each experiment, separating e.g.
    when a lock waited or not.

--- src/test/modules/microbench/lwlock/bench.c
+++ src/test/modules/microbench/lwlock/bench.c
@@ -18,52 +18,29 @@
 #include "utils/builtins.h"
 #include "utils/tuplestore.h"
 
+#include "multiprocessing.h"
 #include "randomize.h"
 #include "timing-magic.h"
+
 
 PG_MODULE_MAGIC;
 
 PG_FUNCTION_INFO_V1(bench_lwlock);
 
-/*
- * bench_lwlock(n, rounds, randomize) -> SETOF
- *     (op text, avg_ns float8, batch_size int8, id int8)
- */
-Datum
-bench_lwlock(PG_FUNCTION_ARGS)
+static void
+run_lwlock_bench(int proc_id, int n_parallel, int rounds, int iterations,
+				 ReturnSetInfo *rsinfo)
 {
-	int64			n = PG_GETARG_INT64(0);
-	int64			rounds = PG_ARGISNULL(1) ? 1 : PG_GETARG_INT64(1);
-	bool			randomize = PG_ARGISNULL(2) ? true : PG_GETARG_BOOL(2);
-	ReturnSetInfo  *rsinfo = (ReturnSetInfo *) fcinfo->resultinfo;
-	LWLock		  **locks;
-	volatile int64	sink PG_USED_FOR_ASSERTS_ONLY = 0;
+	LWLock	  **locks;
+	pg_prng_state rng;
+	volatile int64 sink PG_USED_FOR_ASSERTS_ONLY = 0;
 
-	if (n <= 0 || rounds <= 0)
-		ereport(ERROR,
-				(errcode(ERRCODE_INVALID_PARAMETER_VALUE),
-				 errmsg("n and rounds must be positive")));
+	(void) n_parallel;
 
-	if (n > NUM_BUFFER_PARTITIONS)
-		n = NUM_BUFFER_PARTITIONS;
-
-	InitMaterializedSRF(fcinfo, 0);
-
-	locks = palloc(sizeof(LWLock *) * n);
-	if (randomize)
-	{
-		pg_prng_state rng;
-
-		pg_prng_seed(&rng, 0xDA7ABA5E);
-		for (int i = 0; i < n; i++)
-			locks[i] = BufMappingPartitionLock(i);
-		shuffle_pointers(&rng, (void **) locks, (int) n);
-	}
-	else
-	{
-		for (int i = 0; i < n; i++)
-			locks[i] = BufMappingPartitionLock(0);
-	}
+	locks = palloc(sizeof(LWLock *) * iterations);
+	pg_prng_seed(&rng, 0xDA7ABA5E ^ (uint64) proc_id * 0x5EED);
+	for (int i = 0; i < iterations; i++)
+		locks[i] = BufMappingPartitionLock(i);
 
 	if (!timing_initialized)
 		pg_initialize_timing();
@@ -71,53 +48,88 @@ bench_lwlock(PG_FUNCTION_ARGS)
 	for (int64 r = 0; r < rounds; r++)
 	{
 		INIT_TIMING_SCOPE();
+
+		/*
+		 * Shuffling every round to reduce collision distribution bias
+		 */
+		shuffle_pointers(&rng, (void **) locks, iterations);
 		{
 			BufferDesc *buf_desc = GetBufferDescriptor(1);
-			BEGIN_TIMING("spin-lock", n)
+
+			BEGIN_GROUPED_TIMING("spin-lock", iterations, 2)
+				{
+					LockBufHdr(buf_desc);
+					UnlockBufHdr(buf_desc);
+				}
+			END_GROUPED_TIMING;
+		}
+
+		BEGIN_GROUPED_TIMING("LWLock-ex", iterations, 2)
 			{
-				LockBufHdr(buf_desc);
-				UnlockBufHdr(buf_desc);
+				LWLock	   *lock = locks[i];
+
+				group_id = LWLockAcquire(lock, LW_EXCLUSIVE) ? 0 : 1;
+				LWLockRelease(lock);
 			}
-			END_TIMING;
-		}
+		END_GROUPED_TIMING;
 
-		BEGIN_TIMING("LWLock-ex", n)
-		{
-			LWLock	   *lock = locks[i];
 
-			LWLockAcquire(lock, LW_EXCLUSIVE);
-			LWLockRelease(lock);
-		}
-		END_TIMING;
+		BEGIN_GROUPED_TIMING("LWLock-cond", iterations, 2)
+			{
+				LWLock	   *lock = locks[i];
 
-		BEGIN_TIMING("LWLock-sh", n)
-		{
-			LWLock	   *lock = locks[i];
+				if (LWLockConditionalAcquire(lock, LW_EXCLUSIVE))
+				{
+					group_id = 0;
+					LWLockRelease(lock);
+				}
+				else
+					group_id = 1;
+			}
+		END_GROUPED_TIMING;
 
-			LWLockAcquire(lock, LW_SHARED);
-			LWLockRelease(lock);
-		}
-		END_TIMING;
+		BEGIN_GROUPED_TIMING("nop", iterations, 2)
+			{
+				LWLock	   *lock = locks[i];
 
-		BEGIN_TIMING("LWLock-cond", n)
-		{
-			LWLock	   *lock = locks[i];
-
-			if (!LWLockConditionalAcquire(lock, LW_SHARED))
-				elog(ERROR, "Failed to acquire lock");
-			LWLockRelease(lock);
-		}
-		END_TIMING;
-
-		BEGIN_TIMING("nop", n)
-		{
-			LWLock	   *lock = locks[i];
-
-			sink += (int64) (uintptr_t) lock;
-		}
-		END_TIMING;
+				sink += (int64) (uintptr_t) lock;
+			}
+		END_GROUPED_TIMING;
 	}
 
 	(void) sink;
+}
+
+static void
+microbench_parallel_work(int proc_id, int n_parallel, int rounds, int iterations)
+{
+	run_lwlock_bench(proc_id, n_parallel, rounds, iterations, NULL);
+}
+
+/*
+ * bench_lwlock(n_parallel, rounds, iterations) -> SETOF
+ *     (op text, avg_ns float8, batch_size int8, id int8, group int8)
+ */
+Datum
+bench_lwlock(PG_FUNCTION_ARGS)
+{
+	int			n_parallel = PG_ARGISNULL(0) ? 1 : PG_GETARG_INT32(0);
+	int			rounds = PG_ARGISNULL(1) ? 1 : PG_GETARG_INT64(1);
+	int			iterations = PG_ARGISNULL(2) ? 128 : PG_GETARG_INT64(2);
+	ReturnSetInfo *rsinfo = (ReturnSetInfo *) fcinfo->resultinfo;
+	int			proc_id;
+
+	if (n_parallel <= 0 || rounds <= 0 || iterations <= 0)
+		ereport(ERROR,
+				(errcode(ERRCODE_INVALID_PARAMETER_VALUE),
+				 errmsg("n_parallel, rounds, and iterations must be positive")));
+
+	InitMaterializedSRF(fcinfo, 0);
+
+	proc_id = replicate_backend(n_parallel, rounds, iterations, 2,
+							   microbench_parallel_work);
+	run_lwlock_bench(proc_id, n_parallel, rounds, iterations, rsinfo);
+	microbench_mp_leave(rsinfo);
+
 	return (Datum) 0;
 }
