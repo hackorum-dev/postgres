d2d4591e500c1e76b5198e9fe32d3640ca30661c
Alexandre Felipe <o.alexandre.felipe@gmail.com>
2026-08-27 06:50:32 +0100

    buffmap multi processing benchmark
    This should be in the previous patch...

--- src/test/modules/microbench/microbench--1.0.sql
+++ src/test/modules/microbench/microbench--1.0.sql
@@ -81,6 +81,16 @@ AS $$
   GROUP BY s.op, s."group"
   ORDER BY min(s.id), s."group" NULLS FIRST;
 $$;
+CREATE FUNCTION bench_bufmap(
+	IN n_parallel int4 DEFAULT 1,
+	IN rounds int8 DEFAULT 1,
+	IN iterations int8 DEFAULT 128
+)
+RETURNS SETOF microbench_sample
+AS 'MODULE_PATHNAME', 'bench_bufmap'
+LANGUAGE C;
+
+REVOKE ALL ON FUNCTION bench_bufmap(int4, int8, int8) FROM PUBLIC;
 CREATE FUNCTION bench_lwlock(
 	IN n_parallel int4 DEFAULT 1,
 	IN rounds int8 DEFAULT 1,
