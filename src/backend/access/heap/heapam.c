86e2cf30a1d2dbaaa6823cbb5f2ed39e8d1643d4
David Geier <geidav.pg@gmail.com>
2026-09-03 15:25:09 +0200

    Remove rd_index

--- src/backend/access/heap/heapam.c
+++ src/backend/access/heap/heapam.c
@@ -4423,7 +4423,7 @@ check_lock_if_inplace_updateable_rel(Relation relation,
 				{
 					Relation	irel = index_open(relid, AccessShareLock);
 
-					SET_LOCKTAG_RELATION(tag, dbid, irel->rd_index->indrelid);
+					SET_LOCKTAG_RELATION(tag, dbid, RelationGetIndex(irel)->indrelid);
 					index_close(irel, AccessShareLock);
 				}
 				else
@@ -4477,7 +4477,7 @@ check_inplace_rel_lock(HeapTuple oldtup)
 	{
 		Relation	irel = index_open(relid, AccessShareLock);
 
-		SET_LOCKTAG_RELATION(tag, dbid, irel->rd_index->indrelid);
+		SET_LOCKTAG_RELATION(tag, dbid, RelationGetIndex(irel)->indrelid);
 		index_close(irel, AccessShareLock);
 	}
 	else
