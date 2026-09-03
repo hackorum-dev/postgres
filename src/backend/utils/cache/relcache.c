a91144dbf826deb90ad1e90892d369ab79b270ef
David Geier <geidav.pg@gmail.com>
2026-09-03 10:38:03 +0200

    Remove rd_lockinfo

--- src/backend/utils/cache/relcache.c
+++ src/backend/utils/cache/relcache.c
@@ -1292,11 +1292,6 @@ retry:
 	}
 
 	/*
-	 * initialize the relation lock manager information
-	 */
-	RelationInitLockInfo(relation); /* see lmgr.c */
-
-	/*
 	 * initialize physical addressing information for the relation
 	 */
 	RelationInitPhysicalAddr(relation);
@@ -2034,11 +2029,6 @@ formrdesc(const char *relationName, Oid relationReltype,
 		RelationMapUpdateMap(RelationGetRelid(relation),
 							 RelationGetRelid(relation),
 							 isshared, true);
-
-	/*
-	 * initialize the relation lock manager information
-	 */
-	RelationInitLockInfo(relation); /* see lmgr.c */
 
 	/*
 	 * initialize physical addressing information for the relation
@@ -3768,8 +3758,6 @@ RelationBuildLocalRelation(const char *relname,
 	}
 	else
 		rel->rd_rel->relfilenode = relfilenumber;
-
-	RelationInitLockInfo(rel);	/* see lmgr.c */
 
 	RelationInitPhysicalAddr(rel);
 
@@ -6646,11 +6634,10 @@ load_relcache_init_file(bool shared)
 		rel->pgstat_info = NULL;
 
 		/*
-		 * Recompute lock and physical addressing info.  This is needed in
-		 * case the pg_internal.init file was copied from some other database
-		 * by CREATE DATABASE.
+		 * Recompute physical addressing info.  This is needed in case the
+		 * pg_internal.init file was copied from some other database by
+		 * CREATE DATABASE.
 		 */
-		RelationInitLockInfo(rel);
 		RelationInitPhysicalAddr(rel);
 	}
 
