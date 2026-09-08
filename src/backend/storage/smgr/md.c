03141350b1c9403ff4dbd897156834f532618c25
Peter Eisentraut <peter@eisentraut.org>
2026-09-08 11:42:24 +0200

    Use C11 alignas instead of palloc/malloc for alignment
    Replace several cases where palloc()/malloc()/etc. was used solely to
    obtain an aligned buffer.  Use alignas with a local variable instead.
    
    The previous alignment guarantees are carried over.  palloc-based
    allocations are replaced by alignas(MAXIMUM_ALIGNOF).  Theoretically,
    malloc-based allocations should be replaced by alignas(max_align_t),
    but MSVC doesn't provide max_align_t, and so we use MAXIMUM_ALIGNOF
    here as well.  They should be the same in practice.
    
    The allocations in InitWalRecovery() are not converted, because the
    comment says it is also this way to avoid wasting storage.  The
    comment in XLogReaderAllocate(), on the other hand, was probably
    copied from InitWalRecovery(), but the part of the comment about
    wasting storage does not make sense in that context, so it is
    converted.
    
    FIXME: indent in xlogreader.h

--- src/backend/storage/smgr/md.c
+++ src/backend/storage/smgr/md.c
@@ -1823,13 +1823,11 @@ _mdfd_getseg(SMgrRelation reln, ForkNumber forknum, BlockNumber blkno,
 			 */
 			if (nblocks < ((BlockNumber) RELSEG_SIZE))
 			{
-				char	   *zerobuf = palloc_aligned(BLCKSZ, PG_IO_ALIGN_SIZE,
-													 MCXT_ALLOC_ZERO);
+				alignas(PG_IO_ALIGN_SIZE) const char zerobuf[BLCKSZ] = {0};
 
 				mdextend(reln, forknum,
 						 nextsegno * ((BlockNumber) RELSEG_SIZE) - 1,
 						 zerobuf, skipFsync);
-				pfree(zerobuf);
 			}
 			flags = O_CREAT;
 		}
