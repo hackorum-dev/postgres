ad1216e8270056a2fe7dbd607b3725be1d24af48
Heikki Linnakangas <heikki.linnakangas@iki.fi>
2026-09-18 20:57:26 +0300

    Check that oldestXID and oldestMulti are consistent at pg_upgrade
    Now that pg_upgrade will rewrite multixid members, starting from
    oldestMulti, it's important that oldestMulti is valid.  Add a sanity
    check that oldestMulti is not newer than the oldest datminmxid value
    in pg_database.
    
    One case where this could happen is if the cluster was previously
    upgraded to version 9.3 with a buggy pg_upgrade version that didn't
    have commit a61daa14d5.  This new pg_upgrade check is similar to the
    defence that was added in commit 78db307bb2 to VACUUM to avoid
    truncating away multixids if oldestMulti is too new.  This pg_upgrade
    check differs in that we don't try to soldier on with the upgrade if
    the oldestMultiXID is inconsistent, but rather just abort the upgrade.
    
    Reported-by: Noah Misch <noah@leadboat.com>
    Discussion: https://www.postgresql.org/message-id/20260827231757.78.noahmisch@microsoft.com
    Backpatch-through: 19

--- src/include/access/multixact.h
+++ src/include/access/multixact.h
@@ -110,9 +110,35 @@ extern int	GetMultiXactIdMembers(MultiXactId multi, MultiXactMember **members,
 extern void GetMultiXactInfo(uint32 *multixacts, MultiXactOffset *nextOffset,
 							 MultiXactId *oldestMultiXactId,
 							 MultiXactOffset *oldestOffset);
-extern bool MultiXactIdPrecedes(MultiXactId multi1, MultiXactId multi2);
-extern bool MultiXactIdPrecedesOrEquals(MultiXactId multi1,
-										MultiXactId multi2);
+
+
+/*
+ * Decide which of two MultiXactIds is earlier.
+ *
+ * XXX do we need to do something special for InvalidMultiXactId?
+ * (Doesn't look like it.)
+ */
+static inline bool
+MultiXactIdPrecedes(MultiXactId multi1, MultiXactId multi2)
+{
+	int32		diff = (int32) (multi1 - multi2);
+
+	return (diff < 0);
+}
+
+/*
+ * MultiXactIdPrecedesOrEquals -- is multi1 logically <= multi2?
+ *
+ * XXX do we need to do something special for InvalidMultiXactId?
+ * (Doesn't look like it.)
+ */
+static inline bool
+MultiXactIdPrecedesOrEquals(MultiXactId multi1, MultiXactId multi2)
+{
+	int32		diff = (int32) (multi1 - multi2);
+
+	return (diff <= 0);
+}
 
 extern int	multixactoffsetssyncfiletag(const FileTag *ftag, char *path);
 extern int	multixactmemberssyncfiletag(const FileTag *ftag, char *path);
