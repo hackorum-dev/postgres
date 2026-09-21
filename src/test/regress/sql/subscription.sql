a508d8186a99aba13a0be5e390c3c5faadb25e64
Chao Li (Evan) <lic@highgo.com>
2026-09-21 17:31:14 +0800

    Fix inaccurate inclusive-bound error messages.
    ALTER SUBSCRIPTION ... SKIP accepts a skip LSN equal to the replication
    origin's progress, but its error message said that the skip LSN must be
    greater than the origin LSN. Likewise, ParseVariableDouble() accepts
    values at either end of its documented [min,max] range, but its
    messages said that they had to be strictly inside the range.
    
    Adjust the messages to describe the inclusive bounds accurately, and
    add coverage for both cases.
    
    Suggested-by: Bharath Rupireddy <bharath.rupireddyforpostgres@gmail.com>
    Author: Chao Li <lic@highgo.com>
    Reviewed-by: Kiran Kaki <itskkpg@gmail.com>
    Discussion: https://postgr.es/m/7B8F5F12-98A5-4618-867A-904EA1334FD4@gmail.com

--- src/test/regress/sql/subscription.sql
+++ src/test/regress/sql/subscription.sql
@@ -231,6 +231,18 @@ ALTER SUBSCRIPTION regress_testsub SKIP (lsn = '0/12345');
 -- ok - with lsn = NONE
 ALTER SUBSCRIPTION regress_testsub SKIP (lsn = NONE);
 
+-- fail - LSN must not be behind the replication origin
+RESET SESSION AUTHORIZATION;
+SELECT pg_replication_origin_advance(
+    'pg_' || (SELECT oid FROM pg_subscription WHERE subname = 'regress_testsub'),
+    '0/12346') \gset
+-- ok - LSN equal to the replication origin is accepted
+ALTER SUBSCRIPTION regress_testsub SKIP (lsn = '0/12346');
+ALTER SUBSCRIPTION regress_testsub SKIP (lsn = NONE);
+-- fail - LSN behind the replication origin is rejected
+ALTER SUBSCRIPTION regress_testsub SKIP (lsn = '0/12345');
+SET SESSION AUTHORIZATION regress_subscription_user;
+
 -- fail
 ALTER SUBSCRIPTION regress_testsub SKIP (lsn = '0/0');
 
